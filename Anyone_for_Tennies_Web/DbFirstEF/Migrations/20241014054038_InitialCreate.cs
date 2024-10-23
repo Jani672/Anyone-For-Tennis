5c038b2e9c6d890121cdd21a82a42a5c8e72ab4f
Vaikaran <150897497+Vaikaran@users.noreply.github.com>
2024-10-23 12:21:40 +0930

    Add files via upload

---
+++ Anyone_for_Tennies_Web/DbFirstEF/Migrations/20241014054038_InitialCreate.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace DbFirstEF.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CoachProfiles",
+                columns: table => new
+                {
+                    CoachId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Biography = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CoachProfiles", x => x.CoachId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "CoachProfiles",
+                columns: new[] { "CoachId", "Biography" },
+                values: new object[,]
+                {
+                    { 1, "Biography for Coach 1" },
+                    { 2, "Biography for Coach 2" },
+                    { 3, "Biography for Coach 3" },
+                    { 20, "Biography for Coach 20" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CoachProfiles");
+        }
+    }
+}
---
+++ Anyone_for_Tennies_Web/DbFirstEF/Migrations/20241021140801_SeedMoreCoachProfiles.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace DbFirstEF.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedMoreCoachProfiles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "CoachProfiles",
+                columns: new[] { "CoachId", "Biography" },
+                values: new object[,]
+                {
+                    { 4, "Biography for Coach 4" },
+                    { 5, "Biography for Coach 5" },
+                    { 6, "Biography for Coach 6" },
+                    { 7, "Biography for Coach 7" },
+                    { 8, "Biography for Coach 8" },
+                    { 9, "Biography for Coach 9" },
+                    { 10, "Biography for Coach 10" },
+                    { 11, "Biography for Coach 11" },
+                    { 12, "Biography for Coach 12" },
+                    { 13, "Biography for Coach 13" },
+                    { 14, "Biography for Coach 14" },
+                    { 15, "Biography for Coach 15" },
+                    { 16, "Biography for Coach 16" },
+                    { 17, "Biography for Coach 17" },
+                    { 18, "Biography for Coach 18" },
+                    { 19, "Biography for Coach 19" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "CoachProfiles",
+                keyColumn: "CoachId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "CoachProfiles",
+                keyColumn: "CoachId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "CoachProfiles",
+                keyColumn: "CoachId",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "CoachProfiles",
+                keyColumn: "CoachId",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "CoachProfiles",
+                keyColumn: "CoachId",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "CoachProfiles",
+                keyColumn: "CoachId",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "CoachProfiles",
+                keyColumn: "CoachId",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "CoachProfiles",
+                keyColumn: "CoachId",
+                keyValue: 11);
+
+            migrationBuilder.DeleteData(
+                table: "CoachProfiles",
+                keyColumn: "CoachId",
+                keyValue: 12);
+
+            migrationBuilder.DeleteData(
+                table: "CoachProfiles",
+                keyColumn: "CoachId",
+                keyValue: 13);
+
+            migrationBuilder.DeleteData(
+                table: "CoachProfiles",
+                keyColumn: "CoachId",
+                keyValue: 14);
+
+            migrationBuilder.DeleteData(
+                table: "CoachProfiles",
+                keyColumn: "CoachId",
+                keyValue: 15);
+
+            migrationBuilder.DeleteData(
+                table: "CoachProfiles",
+                keyColumn: "CoachId",
+                keyValue: 16);
+
+            migrationBuilder.DeleteData(
+                table: "CoachProfiles",
+                keyColumn: "CoachId",
+                keyValue: 17);
+
+            migrationBuilder.DeleteData(
+                table: "CoachProfiles",
+                keyColumn: "CoachId",
+                keyValue: 18);
+
+            migrationBuilder.DeleteData(
+                table: "CoachProfiles",
+                keyColumn: "CoachId",
+                keyValue: 19);
+        }
+    }
+}
