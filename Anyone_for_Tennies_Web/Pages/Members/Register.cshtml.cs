dd46000ad4e6c53d35915f6b4308cc1b0e1f123c
Jani672 <135194982+Jani672@users.noreply.github.com>
2024-10-25 09:44:41 +0530

    Create Register Page

---
+++ Anyone_for_Tennies_Web/Pages/Members/Register.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Anyone_for_Tennies.Pages.Members
+{
+    public class RegisterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
