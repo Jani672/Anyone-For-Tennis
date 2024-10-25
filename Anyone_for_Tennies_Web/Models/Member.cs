e920e44655225cf7f2add62ecec4ed91ce048f2a
Jani672 <135194982+Jani672@users.noreply.github.com>
2024-10-25 13:42:13 +0530

    Create Member.cs

---
+++ Anyone_for_Tennies_Web/Models/Member.cs
@@ -0,0 +1,21 @@
+﻿namespace Anyone_for_Tennies.Models
+{
+    public class Member
+    {
+        public int MemberId { get; set; }
+        public int RoleID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Address { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public bool Active { get; set; } = true;
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+
+        public Role Role { get; set; }
+    }
+
+}
