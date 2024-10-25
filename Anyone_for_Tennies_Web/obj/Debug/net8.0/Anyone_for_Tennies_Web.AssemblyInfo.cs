17a7d987a0d93c4da17737c4e8f50c15bd7222f4
Jani672 <135194982+Jani672@users.noreply.github.com>
2024-10-25 09:42:44 +0530

    Create Member Dashboard
    Create Member Dashboard

--- Anyone_for_Tennies_Web/obj/Debug/net8.0/Anyone_for_Tennies_Web.AssemblyInfo.cs
+++ Anyone_for_Tennies_Web/obj/Debug/net8.0/Anyone_for_Tennies_Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Anyone_for_Tennies_Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da51c0bc12fc2fc3cd7a1cf97186d9d5bce50f9f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Anyone_for_Tennies_Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Anyone_for_Tennies_Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
